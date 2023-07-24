39c6979d34746221ed07c85900ace48ff6f4640c
SudaraWishvajith <it21221378@my.sliit.lk>
2023-07-24 20:46:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { Scroll, ScrollControls } from "@react-three/drei";
+
+function App() {
+  return (
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={3} damping={0.1}>
+          <Experience />
+          <Scroll html>
+            <h1 className="text-3xl font-bold underline">
+              Hello world!
+            </h1>
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+
+    </>
+
+  );
+}
+
+export default App;
